dfe8a97b7410e583120717034b861c350bb61268
rjllind <adlekrjllind@gmail.com>
2026-02-01 13:35:26 +0700

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -16,7 +16,7 @@
     // Add your custom code here.
   };
 
-  var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
+  var TITLE = "MKUltra Company: An Alternate History" + '_' + "Rjllind";
 
   // the url is a link to game.json
   // test url: https://aucchen.github.io/social_democracy_mods/v0.1.json
